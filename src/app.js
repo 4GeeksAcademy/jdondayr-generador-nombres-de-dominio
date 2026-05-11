5460ed45b296996b3df5da654b043b4f12b9e6b5
Juan <jdondayr@gmail.com>
2026-05-11 19:06:28 +0200

    Creo lista en HTML para introducir los <li>, y rehago la función. Funcionalidades de eliminar el final del adjetivo si coincide con la extensión, conexión con el DOM, y eliminación de los <li> si se marca el checkbox que de su lado

--- src/app.js
+++ src/app.js
@@ -4,21 +4,37 @@ let sustantivos = ["mapache", "oso", "pescado"];
 let adjetivos = ["burgues", "ingles", "propio", "internet"];
 let extensiones = ["com", "net", "es", "io", "us"];
 
-// Generador de nombres de dominio
-const generadorDeNombresDeDominio = (arrPronombres, arrSustantivos, arrAdjetivos, arrExtensiones) => {
-  for (let pronombre of arrPronombres) {
-    for (let sustantivo of arrSustantivos) {
-      for (let adjetivo of arrAdjetivos) {
-        for (let extension of arrExtensiones) {
-          if (adjetivo.endsWith(extension)) {
-            let adjetivoSeparado = adjetivo.split("");
-            adjetivoSeparado.splice(adjetivo.length - extension.length, extension.length);
-            console.log(pronombre + sustantivo + adjetivoSeparado.join("") + "." + extension);
+// Lista Ul
+let miLista = document.querySelector("#miLista");
+
+window.onload = () => {
+  // Generador de nombres de dominio
+    for (let pronombre of pronombres) {
+      for (let sustantivo of sustantivos) {
+        for (let adjetivo of adjetivos) {
+          for (let extension of extensiones) {
+            // Creo Checkbox
+            let checkBox = document.createElement("input");
+            checkBox.type = "checkbox";
+            // Elimina el elemento si se marca la Check Box
+            checkBox.addEventListener("change", () => miLista.removeChild(liElement))
+
+            // Creo li
+            let liElement = document.createElement("li");
+            // Función eliminadora del final del adjetivo si coincide con extensión
+            if (adjetivo.endsWith(extension)) {
+              let adjetivoSeparado = adjetivo.split("");
+              adjetivoSeparado.splice(adjetivo.length - extension.length, extension.length);
+              liElement.textContent = pronombre + sustantivo + adjetivoSeparado.join("") + "." + extension
+            }
+            else liElement.textContent = pronombre + sustantivo + adjetivo + "." + extension;
+            
+            // Añado ambos elementos
+            liElement.appendChild(checkBox);
+            miLista.appendChild(liElement);
+            
           }
-          else console.log(pronombre + sustantivo + adjetivo + "." + extension);
         }
       }
     }
   }
-}
-generadorDeNombresDeDominio(pronombres, sustantivos, adjetivos, extensiones);
