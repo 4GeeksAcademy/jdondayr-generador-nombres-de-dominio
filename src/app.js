03613ef4bcf46449016dd9bac309846e3bedbbfb
Juan <jdondayr@gmail.com>
2026-05-12 12:16:34 +0200

    Nuevo extra: Creo estructura HTML comlpeta, doy estilos con Bootstrap y CSS, y hago que las listas sean dinámicas e interactivas con JavaScript. El cliente añade a las listas los elementos que quiera, el programa toma de ellas los elementos y genera nombres de dominio mediante el pulsado de un botón. Añado otro botón de reset para limpiar todo.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,57 @@
+// -------------------------------- VARIABLES -----------------------------------------------------
+
+// Botones
+let botonPronombres = document.querySelector("#botonPronombres");
+let botonAdjetivos = document.querySelector("#botonAdjetivos");
+let botonSustantivos = document.querySelector("#botonSustantivos");
+
+// Inputs
+let inputPronombres = document.querySelector("#inputPronombres");
+let inputAdjetivos = document.querySelector("#inputAdjetivos");
+let inputSustantivos = document.querySelector("#inputSustantivos");
+
+// Listas
+let listaPronombres = document.querySelector("#listaPronombres");
+let listaAdjetivos = document.querySelector("#listaAdjetivos");
+let listaSustantivos = document.querySelector("#listaSustantivos");
+
 // Arrays
-let pronombres = ["el", "nuestro", "tu"];
-let sustantivos = ["mapache", "oso", "pescado"];
-let adjetivos = ["burgues", "ingles", "propio", "internet"];
+let pronombres = [];
+let sustantivos = [];
+let adjetivos = [];
 let extensiones = ["com", "net", "es", "io", "us"];
+
+// Botón generador y reset
+let botonGenerador = document.querySelector("#botonGenerador");
+let botonReset = document.querySelector("#reset");
 
 // Lista Ul
 let miLista = document.querySelector("#miLista");
 
-window.onload = () => {
-  // Generador de nombres de dominio
+
+//------------------------------ ACCIONES ---------------------------------------------------------
+
+// Pulsar botón de Añadir a una lista
+const añadirALaLista = (boton, input, lista, array) => {
+  boton.addEventListener("click", () => {
+    // Se crea el li
+    let liElement = document.createElement("li");
+    liElement.textContent = input.value;
+    // Se añade el li
+    lista.appendChild(liElement);
+    // Se añade al array como cadena de texto
+    array.push(String(input.value));
+    // Se vacía el valor del input
+    input.value = "";
+  })
+}
+// Ejecuto la función para cada una de los botones, inputs, listas y arrays
+añadirALaLista(botonPronombres, inputPronombres, listaPronombres, pronombres);
+añadirALaLista(botonAdjetivos, inputAdjetivos, listaAdjetivos, adjetivos);
+añadirALaLista(botonSustantivos, inputSustantivos,listaSustantivos, sustantivos);
+
+// Pulsar botón generador de nombres de dominio
+botonGenerador.addEventListener("click", () => {
     for (let pronombre of pronombres) {
       for (let sustantivo of sustantivos) {
         for (let adjetivo of adjetivos) {
@@ -19,8 +62,9 @@ window.onload = () => {
             // Elimina el elemento si se marca la Check Box
             checkBox.addEventListener("change", () => miLista.removeChild(liElement))
 
-            // Creo li
+            // Creo li y le añado clase elementoLista
             let liElement = document.createElement("li");
+            liElement.classList.add("elementoLista");
             // Función eliminadora del final del adjetivo si coincide con extensión
             if (adjetivo.endsWith(extension)) {
               let adjetivoSeparado = adjetivo.split("");
@@ -32,9 +76,16 @@ window.onload = () => {
             // Añado ambos elementos
             liElement.appendChild(checkBox);
             miLista.appendChild(liElement);
-            
           }
         }
       }
     }
-  }
+})
+
+// Botón Reset que vacía todo
+botonReset.addEventListener("click", () => {
+  miLista.textContent = "";
+  listaPronombres.textContent = ""; inputPronombres.value = "";
+  listaAdjetivos.textContent = ""; inputAdjetivos.value = "";
+  listaSustantivos.textContent = ""; inputSustantivos.value = "";
+});
